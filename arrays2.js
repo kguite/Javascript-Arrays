fc2aa1a8bcdc4d444b60c03aef69f91f08b56415
kguite <kguite@gmail.com>
2021-05-02 18:19:26 -0700

    8kyu Logical Calculator

---
+++ arrays2.js
@@ -0,0 +1,42 @@
+//8kyu Logical Calculator
+
+
+// Your Task
+//Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+//Examples
+// booleans = [True, True, False], operator = "AND"
+// True AND True -> True
+// True AND False -> False
+// return False
+// booleans = [True, True, False], operator = "OR"
+// True OR True -> True
+// True OR False -> True
+// return True
+// booleans = [True, True, False], operator = "XOR"
+// True XOR True -> False
+// False XOR False -> False
+// return False
+// Input
+// an array of Boolean values (1 <= array_length <= 50)
+// a string specifying a logical operator: "AND", "OR", "XOR"
+// Output
+// A Boolean value (True or False).
+
+
+
+
+// First, create object defining the operations.
+// XOR stands for either/or
+
+var ops = {
+	  'AND': (a, b) => a && b,
+	  'OR': (a, b) =>  a || b,
+	  'XOR': (a, b) => a !== b
+	}
+	
+// function takes in array, and an operation
+// uses reduce method.  Reduce method is unusual.  It takes both an item (the array) and a property (op)
+function logicalCalc(array, op){
+	  return array.reduce(ops[op]);
+	}
